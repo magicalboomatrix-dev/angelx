011486c9a6bf717621845c5e5e5afb10600ad000
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2025-10-07 17:20:31 +0530

    Update page.js

--- src/app/login-account/page.js
+++ src/app/login-account/page.js
@@ -150,9 +150,9 @@ export default function LoginAccount() {
               <h3 className="title">
                 <b>Welcome to AngleX</b>
               </h3>
-            <h4 style={{ fontWeight: 'normal', fontSize: '16px', paddingBottom: '10px', color: '#696969' }}>
-  Hope you're doing well!
-</h4>
+            <h4 style={{fontWeight: 'normal',fontSize: '16px',paddingBottom: '10px',color: '#696969'}}>
+                Hope your're doing doing well! 
+              </h4>
 
 
               <div className="form-bx">
@@ -213,6 +213,7 @@ export default function LoginAccount() {
     </div>
   );
 }
+
 
 
 
