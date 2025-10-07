33e0495394dd30764a10a39192a783010b62f09a
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2025-10-07 17:27:16 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,8 @@ export default function Layout({ children }) {
   return (
     <html lang="en">
       <head>
+      <link rel="icon" type="image/x-icon" href="/images/fav.png" />
+
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
@@ -36,3 +38,4 @@ export default function Layout({ children }) {
     </html>
   );
 }
+
