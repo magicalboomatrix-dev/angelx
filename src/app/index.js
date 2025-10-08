a9180609c301a6eb2c9f5680254f1fb9fa31e186
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2025-10-08 12:58:03 +0530

    Update index.js

--- src/app/index.js
+++ src/app/index.js
@@ -46,7 +46,7 @@ export default function Index() {
                 
             </div>
             <div className="right">
-                <a href="https://wa.me/+919876543210?text=Hello%2C%20World!">
+                <a href="https://wa.me/+12366055665?text=Hello%2C%20ANGELX">
                 <Image                
                 src="/images/customer-care.png"
                 alt="customer"
@@ -668,6 +668,7 @@ export default function Index() {
     </div>
   );
 }
+
 
 
 
