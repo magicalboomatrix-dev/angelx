b061f3f207cef8ad60d4081997d27b5016d35d06
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-02-05 21:23:53 +0530

    Update page.js

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,12 +1,40 @@
 "use client";
-import Head from "next/head";
 //import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 import { useRouter } from "next/navigation";
-import {Helmet} from "react-helmet"; 
+export const metadata = {
+  title: "Welcome to AngelX",
+  description:
+    "Log in to AngelX for instant USDT to INR conversions at top rates. Fast processing, secure payouts, reliable support—quickly access your account and sell crypto easily.",
+
+  keywords: [
+    "angelx usdt price",
+    "angelx crypto",
+    "angelx usdt sell",
+    "angelx login",
+    "angelx pro",
+    "angelx pro apk",
+    "angelx exchange",
+  ],
+
+  alternates: {
+    canonical: "https://www.angelx.ind.in/login",
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    maxSnippet: -1,
+    maxImagePreview: "large",
+    maxVideoPreview: -1,
+  },
+
+  authors: [{ name: "AngelX" }],
+  publisher: "AngelX",
+};
 
 
 
