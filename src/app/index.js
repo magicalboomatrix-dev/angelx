dd1403df13ac2a4d0f8030fdc6ff6182a72443d2
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-02-05 16:49:07 +0530

    Update index.js

--- src/app/index.js
+++ src/app/index.js
@@ -2,7 +2,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import Image from "next/image";
 import Link from 'next/link';
-import Head from 'next/head';
 
 import Footer from './components/footer';
 
