1758967d7f0ae14fe5ff60d95fa311d90263d128
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-02-05 14:38:18 +0530

    Update page.js

--- src/app/login-account/page.js
+++ src/app/login-account/page.js
@@ -260,7 +260,7 @@ export default function LoginAccountPage() {
         .prefix {
           display: flex;
           align-items: center;
-          padding: 5px 12px;
+          padding: 7px 12px;
           background: #f8f9fc;
           border-right: 1px solid #e5e7eb;
           min-width: 75px;
