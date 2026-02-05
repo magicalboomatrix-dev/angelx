77b8373de17c86a345ebdc32c3abea1540b685f8
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-02-05 21:11:51 +0530

    Update page.js   exchage discription

--- src/app/exchange/page.js
+++ src/app/exchange/page.js
@@ -1,10 +1,40 @@
 "use client";
-import Head from "next/head";
 import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import {Helmet} from "react-helmet"; 
+
+export const metadata = {
+  title: "Welcome to AngelX — India’s #1 Trusted USDT Exchange Platform",
+  description:
+    "Exchange USDT for INR instantly on AngelX—high rates, fast processing, immediate payouts, strong security, reliable support, and easy crypto selling for seamless trades.",
+
+  keywords: [
+    "angelx usdt price",
+    "angelx crypto",
+    "angelx usdt sell",
+    "angelx login",
+    "angelx pro",
+    "angelx pro apk",
+    "angelx exchange",
+  ],
+
+  alternates: {
+    canonical: "https://www.angelx.ind.in/exchange",
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    maxSnippet: -1,
+    maxImagePreview: "large",
+    maxVideoPreview: -1,
+  },
+
+  authors: [{ name: "AngelX" }],
+  publisher: "AngelX",
+};
+
 
 const Slider = dynamic(
   () => import("react-slick").then((mod) => mod.default),
