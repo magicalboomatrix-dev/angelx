e697c85dac4edb8ab5faf31b45702cdf17d5aa7f
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-02-05 18:39:13 +0530

    Update page.js

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -74,9 +74,9 @@ export default function HomePage() {
     <div>
     
     <Helmet>
-      <title>Welcome to AngelX</title>
-      <meta name="description" content="Log in to AngelX for instant USDT to INR conversions at top rates. Fast processing, secure payouts, reliable support—quickly access your account and sell crypto easily." />
+      <title>Welcome to AngelX</title>      
       <meta name="keywords" content="angelx usdt price, angelx crypto, angelx usdt sell, angelx login, angelx pro, angelx pro apk, angelx exchange" />
+      <meta name="description" content="Log in to AngelX for instant USDT to INR conversions at top rates. Fast processing, secure payouts, reliable support—quickly access your account and sell crypto easily." />
       <link rel="canonical" href="https://www.angelx.ind.in/login" />
       <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"/>
       <meta name="author" content="AngelX" />
