4c0702922fa7b7f6d1f9990d864ea23d18850553
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-03-05 15:03:17 +0530

    Update faq.jsx

--- src/app/components/faq.jsx
+++ src/app/components/faq.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 //import { Link } from "react-router-dom";
 
 export default function FAQ() {
+	
   const [activeIndex, setActiveIndex] = useState(null);
 
   const toggleFAQ = (index) => {
