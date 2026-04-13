2be51d86253907cb9be9a2b432ecf2cd54345a72
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-04-13 15:01:33 +0530

    Update page.js

--- src/app/history/page.js
+++ src/app/history/page.js
@@ -52,7 +52,7 @@ export default function DemoPage() {
           <img src="/images/back-btn.png" alt="back" />
         </Link>
           </div>
-          <h3 className="header-title">Transaction History</h3>
+          <h3 className="header-title">Transaction History ss</h3>
         </div>
 
         <section className="section-1" style={{ background: "#fff" }}>
