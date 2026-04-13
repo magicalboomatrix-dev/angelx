a4bf15153005c068ccdb18a3e21bd234353ab5c2
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-04-13 15:39:26 +0530

    Update page.js

--- src/app/history/page.js
+++ src/app/history/page.js
@@ -48,7 +48,7 @@ export default function DemoPage() {
       <main className="content-wrapper">
         <div className="brdc">
           <div className="back-btn-container">
-            <Link href="/home" className="back-link" style={{position: 'relative',zIndex: '999'}}>
+            <Link href="/USDT-deposit" className="back-link" style={{position: 'relative',zIndex: '999'}}>
           <img src="/images/back-btn.png" alt="back" style={{marginLeft: '0'}} />
         </Link>
           </div>
