53dd4ae70bb0510dbdce4187a5c125859bbbbf96
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-04-14 17:21:03 +0530

    Change favicon from PNG to JPEG format

--- src/app/layout.js
+++ src/app/layout.js
@@ -77,7 +77,7 @@ export default function Layout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/x-icon" href="/images/fav.png" />
+        <link rel="icon" type="image/x-icon" href="/images/fav.jpeg" />
         <link rel="manifest" href="/manifest.json" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
