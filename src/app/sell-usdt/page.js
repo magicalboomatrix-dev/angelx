ade32514095911c824731027c09415d8c2396acf
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-04-14 18:39:10 +0530

    Change currency label from 'Currency' to 'Rupee'

--- src/app/sell-usdt/page.js
+++ src/app/sell-usdt/page.js
@@ -322,7 +322,7 @@ export default function AddBank() {
             <section className="section-2 inner-space" style={{marginBottom: '0' }}>
               <div className="inside">
 				<div className="content-row currency-row">
-				  <span className="field-label">Currency</span>
+				  <span className="field-label">Rupee</span>
 
 				  <div className="currency-tabs">
 					{/* PAYX */}
