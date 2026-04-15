053a47716c6155411e51c515dba3064bb2df2c8e
Aviin <aviinyou07@gmail.com>
2026-04-15 11:44:16 +0530

    updated url

--- userpanel/next.config.mjs
+++ userpanel/next.config.mjs
@@ -107,7 +107,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:4000/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000'}/api/:path*`,
       },
     ];
   },
