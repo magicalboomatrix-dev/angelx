bbb7f1d7a3f922b80c601d5144c46856cd382437
magicalboomatrix-dev <magicalboomatrix@gmail.com>
2026-04-17 15:39:33 +0530

    Update back link to point to withdraw page

--- userpanel/src/app/withdraw-history/page.js
+++ userpanel/src/app/withdraw-history/page.js
@@ -85,7 +85,7 @@ export default function WithdrawHistoryPage() {
       <main className="content-wrapper">
         <div className="brdc">
           <div className="back-btn-container">
-            <Link href="/USDT-deposit" className="back-link" style={{position: 'relative',zIndex: '999'}}>
+            <Link href="/withdraw" className="back-link" style={{position: 'relative',zIndex: '999'}}>
           <img src="/images/back-btn.png" alt="back" style={{marginLeft: '0'}} />
         </Link>
           </div>
